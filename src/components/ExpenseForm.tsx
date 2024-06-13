749f590e47b0a46032ae79f63c7474e2ef75f48f
Vanessa <vanemp21@gmail.com>
2024-06-13 19:08:58 +0200

    feat(add): Added DatePicker package and calendar on project

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -1,5 +1,10 @@
 import { categories } from '../data/categories'
+import DatePicker from 'react-date-picker';
+import 'react-calendar/dist/Calendar.css'
+import 'react-date-picker/dist/DatePicker.css'
+type ValuePiece = Date | null;
 
+type Value = ValuePiece | [ValuePiece, ValuePiece];
 export default function ExpenseForm() {
   return (
  <form className="space-y-5" action="">
@@ -21,6 +26,12 @@ export default function ExpenseForm() {
                 <option key={category.id} value={category.id}>{category.name}</option>
             ))}
         </select>
+    </div>
+    <div className='flex flex-col gap-2'>
+        <label htmlFor="amount" className='text-xl'>Fecha Gasto:</label>
+<DatePicker
+className="bg-slate-100 p-2 border-0"
+/> 
     </div>
     <input type="submit" className='bg-blue-600 cursor-pointer w-full p-2 text-white uppercase font-bold rounded-lg' name="" id="" value={'Registrar Gasto'}/>
 
